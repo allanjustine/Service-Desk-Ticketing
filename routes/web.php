180f190953a4df515db824c9947c5b0af32c6ca5
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-09-01 10:54:59 +0800

    dasgb

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -25,6 +26,7 @@ Route::middleware('auth')->group(function (): void {
     Route::patch('/tickets/{ticket}/urgency', [TicketController::class, 'updateUrgency'])->name('tickets.update-urgency');
     Route::patch('/tickets/{ticket}/status', [TicketController::class, 'updateStatus'])->middleware('it')->name('tickets.update-status');
     Route::middleware('it')->group(function (): void {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
         Route::get('/users', [UserController::class, 'index'])->name('users.index');
         Route::get('/users/{user}/tickets', [UserController::class, 'tickets'])->name('users.tickets');
     });
